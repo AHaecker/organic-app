48d21da92c5d8826caee42d3533d4be330182009
Amanda Haecker <amanda.haecker@uscca.com>
2018-10-17 12:01:08 -0500

    no message

--- src/js/top-posts.js
+++ src/js/top-posts.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
     // Load blog posts
     $.ajax({
-        url: "https://www.usconcealedcarry.com/wp-json/wp/v2/posts?per_page=3",
+        url: "https://www.usconcealedcarry.com/wp-json/wp/v2/posts?per_page=10",
         success: function(data) {
+            var displayCounter = 0;
             $.each(data, function(i, item) {
-                var target = $('#blog-post-' + i);
+                if (item.hasFeaturedVideo == true || displayCounter > 2) {
+                    continue;
+                }
+                var target = $('#blog-post-' + displayCounter);
                 target.find('H5').html(item.title);
                 target.find('IMG').attr('src', item.featuredImage);
                 target.find('A').attr('href', item.link);
                 target.find('.blog-post-author').html(item.author);
+                displayCounter++;
             })
         }
     })
-});
+ });
